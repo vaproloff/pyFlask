2f877318f2796e6eede724f15296e5325b5ca5b8
Konstantin Pivnov <vaproloff@ya.ru>
2023-09-10 21:54:34 +0300

    added lecture 5 working tasks

---
+++ lec_05/main_14.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/message")
+async def read_message():
+    message = {"message": "Hello World"}
+    return JSONResponse(content=message, status_code=200)
---
+++ lec_05/main_16.py
@@ -0,0 +1,40 @@
+import logging
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+@app.get("/")
+async def read_root():
+    logger.info('Отработал GET запрос.')
+    return {"Hello": "World"}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    logger.info('Отработал POST запрос.')
+    return item
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    logger.info(f'Отработал PUT запрос для item id = {item_id}.')
+    return {"item_id": item_id, "item": item}
+
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    logger.info(f'Отработал DELETE запрос для item id = {item_id}.')
+    return {"item_id": item_id}
