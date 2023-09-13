300504acd5b3fbbd467923b1519cb5e687cc77b8
Konstantin Pivnov <vaproloff@ya.ru>
2023-09-13 21:57:45 +0300

    added lecture 6 working tasks

---
+++ lec_06/main_05.py
@@ -0,0 +1,26 @@
+import databases
+import sqlalchemy
+from fastapi import FastAPI
+
+DATABASE_URL = "sqlite:///mydatabase.db"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
+
+...
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
